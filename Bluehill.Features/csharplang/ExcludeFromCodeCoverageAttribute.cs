7f97b0d147746b7f3f22e43b39939b2b6b6fe837
na1307 <na1307@outlook.kr>
2023-01-24 18:42:13 +0900

    Features

---
+++ Bluehill.Features/csharplang/ExcludeFromCodeCoverageAttribute.cs
@@ -0,0 +1,8 @@
+﻿#if !NET5_0_OR_GREATER
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event, Inherited = false, AllowMultiple = false)]
+public sealed class ExcludeFromCodeCoverageAttribute : Attribute {
+    public string? Justification { get; set; }
+}
+#endif
---
+++ Bluehill.Features/csharplang/NullableContextAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Interface | AttributeTargets.Delegate, AllowMultiple = false, Inherited = false)]
+public sealed class NullableContextAttribute : Attribute {
+    public readonly byte Flag;
+
+    public NullableContextAttribute(byte A_1) => Flag = A_1;
+}
