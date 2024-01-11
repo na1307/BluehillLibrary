41b499ceb2d6cd00a190befe481914620a69cc82
na1307 <na1307@outlook.kr>
2024-01-11 11:35:55 +0900

    지역화 제거

--- Bluehill.Forms/AboutBox.cs
+++ Bluehill.Forms/AboutBox.cs
@@ -23,9 +23,9 @@ public sealed class AboutBox : Form {
 
     public AboutBox(Image? logo) {
         InitializeComponent();
-        Text = string.Format(Strings.AboutText, AssemblyTitle);
+        Text = $"{AssemblyTitle} 정보";
         labelProductName.Text = AssemblyProduct;
-        labelVersion.Text = string.Format(Strings.VersionText, AssemblyInformationalVersion);
+        labelVersion.Text = $"버전 {AssemblyInformationalVersion}";
         labelCopyright.Text = AssemblyCopyright;
         labelCompanyName.Text = AssemblyCompany;
         textBoxDescription.Text = AssemblyDescription;
@@ -51,87 +51,152 @@ public sealed class AboutBox : Form {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
-            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.logoPictureBox = new System.Windows.Forms.PictureBox();
-            this.labelProductName = new System.Windows.Forms.Label();
-            this.labelVersion = new System.Windows.Forms.Label();
-            this.labelCopyright = new System.Windows.Forms.Label();
-            this.labelCompanyName = new System.Windows.Forms.Label();
-            this.textBoxDescription = new System.Windows.Forms.TextBox();
-            this.okButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // tableLayoutPanel
-            // 
-            resources.ApplyResources(this.tableLayoutPanel, "tableLayoutPanel");
-            this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 0, 0);
-            this.tableLayoutPanel.Controls.Add(this.labelProductName, 1, 0);
-            this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 1);
-            this.tableLayoutPanel.Controls.Add(this.labelCopyright, 1, 2);
-            this.tableLayoutPanel.Controls.Add(this.labelCompanyName, 1, 3);
-            this.tableLayoutPanel.Controls.Add(this.textBoxDescription, 1, 4);
-            this.tableLayoutPanel.Controls.Add(this.okButton, 1, 5);
-            this.tableLayoutPanel.Name = "tableLayoutPanel";
-            // 
-            // logoPictureBox
-            // 
-            resources.ApplyResources(this.logoPictureBox, "logoPictureBox");
-            this.logoPictureBox.Name = "logoPictureBox";
-            this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
-            this.logoPictureBox.TabStop = false;
-            // 
-            // labelProductName
-            // 
-            resources.ApplyResources(this.labelProductName, "labelProductName");
-            this.labelProductName.Name = "labelProductName";
-            // 
-            // labelVersion
-            // 
-            resources.ApplyResources(this.labelVersion, "labelVersion");
-            this.labelVersion.Name = "labelVersion";
-            // 
-            // labelCopyright
-            // 
-            resources.ApplyResources(this.labelCopyright, "labelCopyright");
-            this.labelCopyright.Name = "labelCopyright";
-            // 
-            // labelCompanyName
-            // 
-            resources.ApplyResources(this.labelCompanyName, "labelCompanyName");
-            this.labelCompanyName.Name = "labelCompanyName";
-            // 
-            // textBoxDescription
-            // 
-            resources.ApplyResources(this.textBoxDescription, "textBoxDescription");
-            this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.ReadOnly = true;
-            this.textBoxDescription.TabStop = false;
-            // 
-            // okButton
-            // 
-            resources.ApplyResources(this.okButton, "okButton");
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Name = "okButton";
-            // 
-            // AboutBox
-            // 
-            this.AcceptButton = this.okButton;
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AboutBox";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.tableLayoutPanel.ResumeLayout(false);
-            this.tableLayoutPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
-            this.ResumeLayout(false);
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
+        tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+        logoPictureBox = new System.Windows.Forms.PictureBox();
+        labelProductName = new System.Windows.Forms.Label();
+        labelVersion = new System.Windows.Forms.Label();
+        labelCopyright = new System.Windows.Forms.Label();
+        labelCompanyName = new System.Windows.Forms.Label();
+        textBoxDescription = new System.Windows.Forms.TextBox();
+        okButton = new System.Windows.Forms.Button();
+        tableLayoutPanel.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)logoPictureBox).BeginInit();
+        SuspendLayout();
+        //
+        // tableLayoutPanel
+        //
+        tableLayoutPanel.ColumnCount = 2;
+        tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+        tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
+        tableLayoutPanel.Controls.Add(logoPictureBox, 0, 0);
+        tableLayoutPanel.Controls.Add(labelProductName, 1, 0);
+        tableLayoutPanel.Controls.Add(labelVersion, 1, 1);
+        tableLayoutPanel.Controls.Add(labelCopyright, 1, 2);
+        tableLayoutPanel.Controls.Add(labelCompanyName, 1, 3);
+        tableLayoutPanel.Controls.Add(textBoxDescription, 1, 4);
+        tableLayoutPanel.Controls.Add(okButton, 1, 5);
+        tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+        tableLayoutPanel.Location = new System.Drawing.Point(10, 8);
+        tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        tableLayoutPanel.Name = "tableLayoutPanel";
+        tableLayoutPanel.RowCount = 6;
+        tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+        tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+        tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+        tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+        tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
+        tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+        tableLayoutPanel.Size = new System.Drawing.Size(487, 245);
+        tableLayoutPanel.TabIndex = 0;
+        //
+        // logoPictureBox
+        //
+        logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+        logoPictureBox.Image = (System.Drawing.Image)resources.GetObject("logoPictureBox.Image");
+        logoPictureBox.Location = new System.Drawing.Point(4, 3);
+        logoPictureBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        logoPictureBox.Name = "logoPictureBox";
+        tableLayoutPanel.SetRowSpan(logoPictureBox, 6);
+        logoPictureBox.Size = new System.Drawing.Size(152, 239);
+        logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+        logoPictureBox.TabIndex = 12;
+        logoPictureBox.TabStop = false;
+        //
+        // labelProductName
+        //
+        labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
+        labelProductName.Location = new System.Drawing.Point(167, 0);
+        labelProductName.Margin = new System.Windows.Forms.Padding(7, 0, 4, 0);
+        labelProductName.MaximumSize = new System.Drawing.Size(0, 16);
+        labelProductName.Name = "labelProductName";
+        labelProductName.Size = new System.Drawing.Size(316, 16);
+        labelProductName.TabIndex = 19;
+        labelProductName.Text = "제품 이름";
+        labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        //
+        // labelVersion
+        //
+        labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+        labelVersion.Location = new System.Drawing.Point(167, 24);
+        labelVersion.Margin = new System.Windows.Forms.Padding(7, 0, 4, 0);
+        labelVersion.MaximumSize = new System.Drawing.Size(0, 16);
+        labelVersion.Name = "labelVersion";
+        labelVersion.Size = new System.Drawing.Size(316, 16);
+        labelVersion.TabIndex = 0;
+        labelVersion.Text = "버전";
+        labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        //
+        // labelCopyright
+        //
+        labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
+        labelCopyright.Location = new System.Drawing.Point(167, 48);
+        labelCopyright.Margin = new System.Windows.Forms.Padding(7, 0, 4, 0);
+        labelCopyright.MaximumSize = new System.Drawing.Size(0, 16);
+        labelCopyright.Name = "labelCopyright";
+        labelCopyright.Size = new System.Drawing.Size(316, 16);
+        labelCopyright.TabIndex = 21;
+        labelCopyright.Text = "저작권";
+        labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        //
+        // labelCompanyName
+        //
+        labelCompanyName.Dock = System.Windows.Forms.DockStyle.Fill;
+        labelCompanyName.Location = new System.Drawing.Point(167, 72);
+        labelCompanyName.Margin = new System.Windows.Forms.Padding(7, 0, 4, 0);
+        labelCompanyName.MaximumSize = new System.Drawing.Size(0, 16);
+        labelCompanyName.Name = "labelCompanyName";
+        labelCompanyName.Size = new System.Drawing.Size(316, 16);
+        labelCompanyName.TabIndex = 22;
+        labelCompanyName.Text = "회사 이름";
+        labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        //
+        // textBoxDescription
+        //
+        textBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+        textBoxDescription.Location = new System.Drawing.Point(167, 99);
+        textBoxDescription.Margin = new System.Windows.Forms.Padding(7, 3, 4, 3);
+        textBoxDescription.Multiline = true;
+        textBoxDescription.Name = "textBoxDescription";
+        textBoxDescription.ReadOnly = true;
+        textBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+        textBoxDescription.Size = new System.Drawing.Size(316, 104);
+        textBoxDescription.TabIndex = 23;
+        textBoxDescription.TabStop = false;
+        textBoxDescription.Text = "설명";
+        //
+        // okButton
+        //
+        okButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+        okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        okButton.Location = new System.Drawing.Point(395, 217);
+        okButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        okButton.Name = "okButton";
+        okButton.Size = new System.Drawing.Size(88, 25);
+        okButton.TabIndex = 24;
+        okButton.Text = "확인(&O)";
+        //
+        // AboutBox
+        //
+        AcceptButton = okButton;
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(507, 261);
+        Controls.Add(tableLayoutPanel);
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        MaximizeBox = false;
+        MinimizeBox = false;
+        Name = "AboutBox";
+        Padding = new System.Windows.Forms.Padding(10, 8, 10, 8);
+        ShowIcon = false;
+        ShowInTaskbar = false;
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+        Text = "AboutBox";
+        tableLayoutPanel.ResumeLayout(false);
+        tableLayoutPanel.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)logoPictureBox).EndInit();
+        ResumeLayout(false);
 
     }
 
--- Bluehill.Forms/Dialog.cs
+++ Bluehill.Forms/Dialog.cs
@@ -53,7 +53,6 @@ public class Dialog : Form {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dialog));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
@@ -62,24 +61,43 @@ public class Dialog : Form {
             // 
             // tableLayoutPanel1
             // 
-            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.okButton, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.cancelButton, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(101, 218);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(170, 31);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // okButton
             // 
-            resources.ApplyResources(this.okButton, "okButton");
+            this.okButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(4, 3);
+            this.okButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(77, 25);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "확인";
             this.okButton.UseVisualStyleBackColor = true;
             this.okButton.Click += new System.EventHandler(this.OK_Button_Click);
             // 
             // cancelButton
             // 
-            resources.ApplyResources(this.cancelButton, "cancelButton");
+            this.cancelButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(89, 3);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(77, 25);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "취소";
             this.cancelButton.UseVisualStyleBackColor = true;
             this.cancelButton.Click += new System.EventHandler(this.Cancel_Button_Click);
             // 
@@ -88,13 +106,15 @@ public class Dialog : Form {
             this.AcceptButton = this.okButton;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.CancelButton = this.cancelButton;
-            resources.ApplyResources(this, "$this");
+            this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Dialog";
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.TopMost = true;
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
